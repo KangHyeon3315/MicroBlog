acc70777101b319326c9293cf7d6cad0311bc942
KangHyeon3315 <son2786w@gmail.com>
2023-04-05 22:54:06 +0900

    test: Flux의 Rest Docs 작성을 위한 WebTestClient로 테스트 코드 수정

--- microservices/composite/src/test/java/com/microblog/composite/CompositeControllerImplTest.java
+++ microservices/composite/src/test/java/com/microblog/composite/CompositeControllerImplTest.java
@@ -3,96 +3,104 @@ package com.microblog.composite;
 import com.microblog.composite.controller.CompositeControllerImpl;
 import core.comment.Comment;
 import core.post.Post;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 
-
-@WebMvcTest(CompositeControllerImpl.class)
-@AutoConfigureRestDocs
+@WebFluxTest
+@ExtendWith(RestDocumentationExtension.class)
+@ActiveProfiles("test")
 public class CompositeControllerImplTest {
-    @MockBean
-    RestTemplate restTemplate;
 
     @MockBean
-    private ConfigServicePropertySourceLocator configServicePropertySourceLocator;
+    WebClient.Builder webClientBuilder;
+
     @Autowired
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    MockMvc mockMvc;
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        webTestClient = WebTestClient.bindToController(new CompositeControllerImpl(webClientBuilder))
+                .configureClient()
+                .filter(WebTestClientRestDocumentation.documentationConfiguration(restDocumentation))
+                .build();
+    }
 
     @Test
-    public void getPostTest() throws Exception {
+    public void getPostTest() {
         int postId = 12;
-        when(restTemplate.getForObject(ArgumentMatchers.anyString(), ArgumentMatchers.eq(Post.class)))
-                .thenReturn(new Post(postId, "post title " + postId, "post author " + postId, "contents " + postId));
-        when(
-                restTemplate.exchange(
-                        ArgumentMatchers.anyString(),
-                        ArgumentMatchers.eq(HttpMethod.GET),
-                        ArgumentMatchers.isNull(),
-                        ArgumentMatchers.eq(new ParameterizedTypeReference<List<Comment>>() {
-                        })
-                )
-        ).thenReturn(
-                ResponseEntity.of(Optional.of(
-                        List.of(
-                                new Comment(postId, 1, "작성자1", "내용1"),
-                                new Comment(postId, 2, "작성자2", "내용2")
-                        )
-                ))
+        Post post = new Post(postId, "post title " + postId, "post author " + postId, "contents " + postId);
+        List<Comment> comments = List.of(
+                new Comment(postId, 1, "작성자1", "내용1"),
+                new Comment(postId, 2, "작성자2", "내용2")
         );
 
-        // get("/composite/" + postId))
-        mockMvc.perform(RestDocumentationRequestBuilders.get("/composite/{postId}", postId))
-                // .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(
-                        document("composite",
-                                // Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
-                                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
-//                                requestFields(
-//                                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("post id")
-//                                ),
-                                pathParameters(
-                                        parameterWithName("postId").description("post id")
-                                ),
-                                responseFields(
-                                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("post id"),
-                                        fieldWithPath("title").type(JsonFieldType.STRING).description("post title"),
-                                        fieldWithPath("author").type(JsonFieldType.STRING).description("post author"),
-                                        fieldWithPath("contents").type(JsonFieldType.STRING).description("post contents"),
-                                        fieldWithPath("comments").type(JsonFieldType.ARRAY).description("post comments"),
-                                        fieldWithPath("comments[].postId").type(JsonFieldType.NUMBER).description("comment's post id"),
-                                        fieldWithPath("comments[].commentId").type(JsonFieldType.NUMBER).description("comment id"),
-                                        fieldWithPath("comments[].author").type(JsonFieldType.STRING).description("comment author"),
-                                        fieldWithPath("comments[].comment").type(JsonFieldType.STRING).description("comment")
-                                )
+        WebClient webClient = Mockito.mock(WebClient.class, Answers.RETURNS_DEEP_STUBS);
+        when(webClientBuilder.build()).thenReturn(webClient);
+        when(
+                webClient.get()
+                        .uri(ArgumentMatchers.anyString())
+                        .retrieve()
+                        .bodyToMono(Post.class)
+                        .log()
+        ).thenReturn(Mono.just(post));
 
+        when(
+                webClient.get()
+                        .uri(ArgumentMatchers.anyString())
+                        .retrieve()
+                        .bodyToFlux(Comment.class)
+                        .log()
+        ).thenReturn(Flux.fromIterable(comments));
+
+        webTestClient.get()
+                .uri("/composite/{postId}", postId)
+                .attribute("org.springframework.restdocs.urlTemplate", "/composite/{postId}")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(document("composite",
+                        pathParameters(
+                                RequestDocumentation.parameterWithName("postId").description("post id")
+                        ),
+                        responseFields(
+                                fieldWithPath("postId").type(JsonFieldType.NUMBER).description("post id"),
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("post title"),
+                                fieldWithPath("author").type(JsonFieldType.STRING).description("post author"),
+                                fieldWithPath("contents").type(JsonFieldType.STRING).description("post contents"),
+                                fieldWithPath("comments").type(JsonFieldType.ARRAY).description("post comments"),
+                                fieldWithPath("comments[].postId").type(JsonFieldType.NUMBER).description("comment's post id"),
+                                fieldWithPath("comments[].commentId").type(JsonFieldType.NUMBER).description("comment id"),
+                                fieldWithPath("comments[].author").type(JsonFieldType.STRING).description("comment author"),
+                                fieldWithPath("comments[].comment").type(JsonFieldType.STRING).description("comment")
                         )
-                );
+                ));
+
     }
 
 }
+
